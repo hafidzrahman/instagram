e7fc61aaaf910c4aa49180c71121b873412ea7bd
hafidzrahmanm <hafidzrahman559@gmail.com>
2025-08-24 18:22:34 +0700

    layout updated

--- app/page.tsx
+++ app/page.tsx
@@ -138,39 +138,41 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <footer className="text-xs text-[#a8a8a8]">
-        <div className="flex flex-wrap justify-center gap-5 mb-8 leading-1">
-          <a href="https://about.meta.com/">Meta</a>
-          <a href="https://about.instagram.com/">About</a>
-          <a href="https://about.instagram.com/blog/">Blog</a>
-          <a href="https://about.instagram.com/about-us/careers">Jobs</a>
-          <a href="https://help.instagram.com/">Help</a>
-          <a href="https://developers.facebook.com/docs/instagram">API</a>
-          <a href="https://www.instagram.com/legal/privacy/">Privacy</a>
-          <a href="https://www.instagram.com/legal/terms/">Terms</a>
-          <a href="https://www.instagram.com/explore/locations/">Locations</a>
-          <a href="https://www.instagram.com/web/lite/">Instagram Lite</a>
-          <a href="https://www.meta.ai/?utm_source=foa_web_footer">Meta AI</a>
-          <a href="https://www.meta.ai/pages/what-is-labubu/?utm_source=foa_web_footer">
-            Meta AI Articles
-          </a>
-          <a href="https://www.threads.com/">Threads</a>
-          <a href="https://www.facebook.com/help/instagram/261704639352628">
-            Contact Uploading & Non-Users
-          </a>
-          <a href="https://www.instagram.com/accounts/meta_verified/?entrypoint=web_footer">
-            Meta Verified
-          </a>
-          <a href="https://indonesia.fb.com/panduan-digital/">
-            Meta in Indonesia
-          </a>
-        </div>
-        <div className="flex justify-center gap-4">
-          <select name="language" id="language">
-            <option value="">English</option>
-            <option value="">Indonesia</option>
-          </select>
-          <p>&copy; 2025 Instagram from Meta</p>
+      <footer className="flex justify-center items-center text-xs text-[#a8a8a8] mb-16">
+        <div>
+          <div className="flex flex-wrap justify-center gap-5 mb-8 leading-1">
+            <a href="https://about.meta.com/">Meta</a>
+            <a href="https://about.instagram.com/">About</a>
+            <a href="https://about.instagram.com/blog/">Blog</a>
+            <a href="https://about.instagram.com/about-us/careers">Jobs</a>
+            <a href="https://help.instagram.com/">Help</a>
+            <a href="https://developers.facebook.com/docs/instagram">API</a>
+            <a href="https://www.instagram.com/legal/privacy/">Privacy</a>
+            <a href="https://www.instagram.com/legal/terms/">Terms</a>
+            <a href="https://www.instagram.com/explore/locations/">Locations</a>
+            <a href="https://www.instagram.com/web/lite/">Instagram Lite</a>
+            <a href="https://www.meta.ai/?utm_source=foa_web_footer">Meta AI</a>
+            <a href="https://www.meta.ai/pages/what-is-labubu/?utm_source=foa_web_footer">
+              Meta AI Articles
+            </a>
+            <a href="https://www.threads.com/">Threads</a>
+            <a href="https://www.facebook.com/help/instagram/261704639352628">
+              Contact Uploading & Non-Users
+            </a>
+            <a href="https://www.instagram.com/accounts/meta_verified/?entrypoint=web_footer">
+              Meta Verified
+            </a>
+            <a href="https://indonesia.fb.com/panduan-digital/">
+              Meta in Indonesia
+            </a>
+          </div>
+          <div className="flex justify-center gap-4">
+            <select name="language" id="language">
+              <option value="">English</option>
+              <option value="">Indonesia</option>
+            </select>
+            <p>&copy; 2025 Instagram from Meta</p>
+          </div>
         </div>
       </footer>
     </div>
